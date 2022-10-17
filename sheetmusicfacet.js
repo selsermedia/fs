fe34e0ca73cbdce4d4b6d3a8ed9ddb52c9aa642f
smJeffS <jselser@selsermedia.com>
2022-10-17 15:01:12 -0400

    create sheetmusicfacet

---
+++ sheetmusicfacet.js
@@ -0,0 +1,5 @@
+wpgb.facets.on('loaded', function(facets) {
+    addEventListener('click', (event) => {
+        document.querySelector(".term-137 ul.wpgb-hierarchical-list li:first-of-type")
+    });
+});
